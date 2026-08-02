7e587322669a4c69fd711291ef83c087845b7897
africoding <nliaustemac@yahoo.com>
2026-08-02 15:57:54 +0100

    connect profile row with signUp function

--- services/auth.ts
+++ services/auth.ts
@@ -11,6 +11,9 @@ export async function signUp(email: string, password: string) {
     return { success: false, message: error.message };
   }
 
+  const profileResult = await createProfile(data.user?.id ?? '');
+  console.log('Profile creation:', profileResult.message);
+
   console.log('Signup successful:', data.user?.email);
   return { success: true, message: 'Account created!' };
 }
@@ -28,4 +31,16 @@ export async function signIn(email: string, password: string) {
 
   console.log('signIn successful:', data.user?.email);
   return { success: true, message: 'login successful' };
+}
+
+export async function createProfile(id: string) {
+  const { data, error } = await supabase.from('profiles').insert({ id: id }).select();
+
+  if (error) {
+    console.log('profiles failed:', error.message);
+    return { success: false, message: error.message };
+  }
+
+  console.log('profiles created:', data[0].id);
+  return { success: true, message: 'create profile successful' };
 }
